fccf4659b44a0851a4e473fd9883309b9671bd8d
Siwei <shensiwei@sina.com>
2015-01-20 06:56:34 +0000

    1.每页900篇文章 2.使用了北京时间

--- app/controllers/refinery/blog/categories_controller.rb
+++ app/controllers/refinery/blog/categories_controller.rb
@@ -6,7 +6,7 @@ module Refinery
       def show
         @category = Refinery::Blog::Category.find(params[:id])
         @posts = @category.posts.live.includes(:comments, :categories).
-          page(params[:page]).per_page(30)
+          page(params[:page]).per_page(900)
       end
 
     end
