04c6fe895b955af0fe8efc4200593306bd01ff46
Siwei <shensiwei@sina.com>
2015-08-24 00:07:09 +0000

    验证了百度token, 现在是２０００每页，搜索页面自动聚焦

--- app/controllers/refinery/blog/categories_controller.rb
+++ app/controllers/refinery/blog/categories_controller.rb
@@ -6,10 +6,10 @@ module Refinery
       def show
         @category = Refinery::Blog::Category.find(params[:id])
         @posts = @category.posts.
-          where('title like ?', "%#{params[:title]}%").
+          where('title like ? or body like ?', "%#{params[:title]}%", "%#{params[:title]}%").
           live.
           includes(:comments, :categories).
-          page(params[:page]).per_page(900)
+          page(params[:page]).per_page(2000)
       end
 
     end
